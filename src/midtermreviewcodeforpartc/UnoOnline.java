1799218882b31f615faa435833538dded9ef8d5e
vinz angeles <angelevi@sheridancollege.ca>
2022-03-11 15:26:24 -0500

    Assignment done!

--- src/midtermreviewcodeforpartc/UnoOnline.java
+++ src/midtermreviewcodeforpartc/UnoOnline.java
@@ -25,6 +25,9 @@ public class UnoOnline
      */
     public static void main(String[] args) 
      {
+       PasswordVal passW = new PasswordVal();
+       passW.validation();
+       
        UnoOnline newPortal = new UnoOnline();
        newPortal.run();
     }
@@ -38,35 +41,12 @@ public class UnoOnline
      */
     private void run()
     {
-        int userCount=0;//keep track of number of users for array
-        Scanner sc = new Scanner(System.in);
+        Scanner scant = new Scanner(System.in);
+        int userCount=0;
+        //keep track of number of users for array
         System.out.println("please enter your desired user name:");
-        String userName = sc.nextLine();
-        boolean validPassword=false;
-        String password="";
-        while(!validPassword)
-        {
-            System.out.println("Passwords must have at least 8 characters");
-            System.out.println("Passwords must have at least one special character");
-            System.out.println("Please enter your desired password:"); 
-            
-            password = sc.nextLine();
-            int specialCharCount=0;
-            //iterate over each character to see if it is a special character
-            for(int i=0;i<password.length(); i++)
-            {
-                if(!(Character.isLetterOrDigit(password.charAt(i))))
-                {
-                    //now we know there is at least one special character
-                    specialCharCount++;
-                }
-            }
-            if(specialCharCount>0 &&password.length()>7)
-            {
-                validPassword=true;
-            }
-        }//loop only ends when password is valid so now we create the User
-        
+        String userName = scant.nextLine();
+        String password = "";
         User newUser = new User(userName, password);
         users[userCount] = newUser;//add the new user to the user list
         userCount++;
